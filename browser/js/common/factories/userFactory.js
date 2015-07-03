6f41f17f4547ead43d80400a2c3f38b079f00a99
Tremayne <trey@trey-mbp.home>
2015-07-03 17:57:48 -0400

    basic collaboration setup

--- browser/js/common/factories/userFactory.js
+++ browser/js/common/factories/userFactory.js
@@ -4,6 +4,16 @@ app.factory('UserFactory', function($http) {
             return $http.post('/register', newInfo).then(function(response) {
                 return response.data;
             });
+        },
+        addMember: function(projectid, userEmail){
+        	return $http.post('/api/users/' + projectid, {email: userEmail}).then(function(response) {
+                return response.data;
+            });
+        },
+        getMembers: function(projectid){        	
+        	return $http.get('/api/users/' + projectid).then(function(response) {
+                return response.data;
+            });
         }
     };
 });
