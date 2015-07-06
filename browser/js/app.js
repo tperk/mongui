886ebe55f007e2724d83bd8c63f186fc68b2bca5
Tyler Perkins <threeperkins@gmail.com>
2015-07-05 21:25:24 -0400

    navbar

--- browser/js/app.js
+++ browser/js/app.js
@@ -6,20 +6,37 @@ app.config(function ($urlRouterProvider, $locationProvider, $mdThemingProvider) 
     $locationProvider.html5Mode(true);
     // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
     $urlRouterProvider.otherwise('/');
-    var logoGreen = $mdThemingProvider.extendPalette('green', {
-        '500': '7fff00'
-    })
-    $mdThemingProvider.definePalette('logoGreen', logoGreen);
+    $mdThemingProvider.definePalette('mongui', {
+        '50': 'fbc93d',
+        '100': '6cc0e5',
+        '200': 'fb4f4f',
+        '300': '202A25',
+        '400': '454851',
+        '500': 'f44336',
+        '600': 'e53935',
+        '700': 'd32f2f',
+        '800': 'c62828',
+        '900': 'b71c1c',
+        'A100': 'ff8a80',
+        'A200': 'ff5252',
+        'A400': 'ff1744',
+        'A700': 'd50000',
+        'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+                                            // on this palette should be dark or light
+        'contrastDarkColors': ['50', '100', '200']
+    });
     $mdThemingProvider.theme('default')
-        .primaryPalette('blue-grey', {
-            'default': '900'
+        .primaryPalette('mongui', {
+            'default': '100'
         })
-        .accentPalette('logoGreen')
-        .warnPalette('red', {
-            'default':'100'
+        .accentPalette('mongui', {
+            'default': '200'
         })
-        .backgroundPalette('blue', {
-            'default': '900'
+        .warnPalette('mongui', {
+            'default': '50'
+        })
+        .backgroundPalette('mongui', {
+            'default': '400'
         })
 });
 
